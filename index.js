1b3e93f33e874b4a6f32803c50786dc94add8076
Shabeena <shabeenakhoja@gmail.com>
2017-05-18 23:24:03 -0700

    working water recommendation feature

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function onIntent(intentRequest, session, callback) {
     var intentName = intentRequest.intent.name;
 
     // dispatch custom intents to handlers here
-    if ("LaunchGrandaughterIntent" == intentName) {
+    if ("AskGrandaughter" == intentName) {
         setNewUserRequest(intent, session, callback);
     }
     else if ("GetMotivationIntent" == intentName) {
@@ -77,6 +77,9 @@ function onIntent(intentRequest, session, callback) {
     else if ("WaterLeftIntent" == intentName) {
         handleWaterLeftRequest(intent, session, callback);
     } 
+    else if ("ReadAppIntent" == intentName) {
+        handleReadAppointment(intent, session, callback);
+    }
     else if ("AMAZON.YesIntent" === intentName) {
         handleMotivationRequest(intent, session, callback);
     } else if ("AMAZON.HelpIntent" === intentName) {
@@ -114,6 +117,12 @@ function getWelcomeResponse(callback) {
 function setNewUserRequest(intent, session, callback)
 {
     console.log("inside setNewUserRequest");
+    var speechOutput = "";
+    speechOutput = "Welcome to GrandDaughter.Let's set your user profile";
+    callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, "some_Reprompt", true));
+    speechOutput = "Please help me with the default username for this device";
+    callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, "some_Reprompt", true));
+
     // var waterintake = 12;
     // var speechOutput = "Your water intake for today should be " + waterintake+ " glasses with each glass being 16 ounces each";
                 
@@ -168,6 +177,36 @@ function handleAppointmentRequest(intent, session, callback) {
         console.log("ending handleAppointmentRequest");
 }
  
+ function handleReadAppointment(intent, session, callback) {
+    console.log("inside ReadAppIntent");
+    var table = "Appointments";
+    var today = new Date();
+    var currentdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    console.log('day is '+currentdate)
+
+    var params = {
+        TableName : table,
+        Key:{
+            "Date" : currentdate
+        }
+    };
+
+    docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        var speechOutput1 = "You have no appointments today";
+        var reprompt1 = "Do you want me to repeat that for you?";
+        callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput1, reprompt1, false));
+    } else {
+        console.log("Appointment data :", JSON.stringify(data,null, 2));
+        var speechOutput = "You have " + data.Item.AppointmentType + "appointment today at " +data.Item.Time;
+        var reprompt = "Do you want me to repeat that for you?";
+        callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, reprompt, false));
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+    }
+});
+}
+ 
 function handleWaterRecommendRequest(intent, session, callback) {
     console.log("inside handleWaterRecommendRequest");
     
@@ -188,19 +227,29 @@ function handleWaterRecommendRequest(intent, session, callback) {
             var waterintake = Number(data.Item.Weight)*0.5;
             var noofglasses = Math.round(waterintake / 16);
 
-            var dateFormat = require('dateformat');
-            var now = new Date();
+            var today = new Date();
+            var currentdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            console.log(currentdate);
             
-
-
             var params = {
-                'Name'  :  data.Item.Name,
-                'Date'  : "2017-05-05",
-                'WaterToBeConsumed' : noofglasses,
-                'WaterConsumed' : 0,
+                Item: {
+                Date: currentdate,
+                Name: data.Item.Name,
+                WaterConsumed: "0",
+                WaterToBeConsumed: noofglasses
+                },
+
+                TableName: "WaterLogIntake"  
             }; 
 
             insertRecord(params,"WaterRecommendation",callback);
+            docClient.put(params, function(err, data) {
+            if (err) {
+                console.log("Error occured in insertRecord", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Put succeeded:", JSON.stringify(data, null, 2));
+            }
+            });
 
             var speechOutput = "Your water intake for today should be " + noofglasses+ " glasses with each glass being 16 ounces each";
             var reprompt = "Do you want me to repeat that for you?";
@@ -217,31 +266,75 @@ function handleWaterConsumptionRequest(intent, session, callback) {
     console.log("inside handleWaterConsumptionRequest");
      if(intent.slots.Water_Count.value !== undefined) {        
             var watercount = intent.slots.Water_Count.value;
-                        
+            var today = new Date();            
             console.log('water count'+ watercount);
 
             //update in the waterlogintake table
+            var params = {
+            TableName:"WaterLogIntake",
+            Key:{
+                "Date": today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+            },
+
+            UpdateExpression: "set WaterConsumed = :r",
+            ExpressionAttributeValues:{
+                ":r":watercount
+            },
+            ReturnValues:"UPDATED_NEW"
+            };
+
+            console.log("Updating the item...");
+            docClient.update(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+                }
+            });
             
-        } else {
-            var speechOutput = "You have to specify the appointment and the date. "+
-                "Please try again. You can say, doctor appointment on January first.";
-            var reprompt = "Please say your appointment.";
-            callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, reprompt, false));
-        }
+            } else {
+                var speechOutput = "You have to specify the appointment and the date. "+
+                    "Please try again. You can say, doctor appointment on January first.";
+                var reprompt = "Please say your appointment.";
+                callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, reprompt, false));
+            }
         console.log("ending handleAppointmentRequest");
 }
 
 function handleWaterLeftRequest(intent, session, callback) {
     console.log("inside handleWaterLeftRequest");
-    //read how much water the user has drank for the current date
-    var waterconsumed = 8;
-    var tobeconsumed = 12 ;
-     var speechOutput = "You have consumed "+waterconsumed+" glasses of water but you need to consume "+tobeconsumed+" more glasses of water for the day";
-     var reprompt = "Do you want me to repeat that for you";
-     
-     callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, reprompt, false));
-     console.log("ending handleWaterLeftRequest");
+    var table = "WaterLogIntake";
+    var today = new Date();
+    var currentdate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    var params = {
+        TableName : table,
+        Key:{
+            "Date" : currentdate
+        }
+    };
+
+    docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+            var waterLeftme = data.Item.WaterToBeConsumed - data.Item.WaterConsumed;
+            console.log("waterLeftme:", waterLeftme);
+            if(waterLeftme <= 0){ 
+                      var speechOutput1 = "Great Job, You've met your daily limit.";
+                      var reprompt1 = "Do you want me to repeat that for you?";
+                      callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput1, reprompt1, false));
+            } else {
+                var speechOutput = "To meet today's limit you need to drink " + waterLeftme+ " more glasses of water, with each glass being 16 ounces";
+                var reprompt = "Do you want me to repeat that for you?";
+                callback(session.attributes, buildSpeechletResponse("some_Header", speechOutput, reprompt, false));
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            }
+            
+    }
+});
 }
+
 
 function insertRecord(data,request,callback)
 {
@@ -257,14 +350,14 @@ function insertRecord(data,request,callback)
         TableName: "Appointments"
     };
     }
-    else if (request == "WaterRecommendation"){
+    if (request == "WaterRecommendation"){
         console.log('entering insertRecord for WaterRecommendation')
         params = {
             Item: {
                 Name: data.Name,
-                Date: "2017-05-05",
-                WaterConsumed: data.WaterConsumed,
-                WaterToBeConsumed: data.WaterToBeConsumed,
+                // Date: "2017-05-05",
+                // WaterConsumed: data.WaterConsumed,
+                // WaterToBeConsumed: data.WaterToBeConsumed,
                 
             },
 
