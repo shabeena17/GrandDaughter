73e56efcf863ce0f0f321575899572db29e71d5f
Rohit Jacob <rj_92@live.com>
2017-06-07 20:55:08 -0700

    Email - port change

--- emailReader.py
+++ emailReader.py
@@ -250,4 +250,4 @@ api.add_resource(emailReader, '/email')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
